44ed79a339211bb87645c8c5eaa6462657909ee5
halag66 <hala.gr11@gmail.com>
2021-10-17 01:36:42 +0300

    app update and initial dockerfile

--- bitcoin-app.py
+++ bitcoin-app.py
@@ -1,3 +1,4 @@
+from typing import Counter
 from flask import Flask
 import requests
 import time
@@ -5,36 +6,11 @@ from collections import deque
 # Description: get the cuurent Bitcoin currency and the avrage for the last 10 min
 # desplay data as a web app
 
+# global arguments
+start_time = time.time()
+total = 0
+counter = 0
 
-app = Flask(__name__)
-#add argument latest price to app inorder to update price if changed
-app.config['latest_price'] = '-1' 
-app.config['counter'] = 0
-
-@app.route("/") # att root display 
-def main():
-    mesurmentsQueue = deque([]) # que to save last 10 bitcoin price value
-    while True:
-        # keep cheking if bitcoin price changed
-        if app.config['latest_price'] != get_latest_bitcoin_price(): # update argumanet if changed
-            app.config['latest_price'] = get_latest_bitcoin_price()
-        output = "The current Price of BitCoin is: " + app.config['latest_price'] + " US $ <br/><br/>"
-        # if first time cheking return avg as cuurent price
-        if app.config['counter'] == 0 : 
-            output += "BitCoin Average price for the last 10 minutes: " + app.config['latest_price'] + " US $"
-            return output
-        # if not 1'st time calculate avg and wait 1 min before returning output
-        else :
-            if(len(mesurmentsQueue)==10): # if 10 minutes passed pop fierst in value
-                mesurmentsQueue.popleft()
-            mesurmentsQueue.append(float(app.config['latest_price']))
-            # update counter
-            app.config['counter'] += 1
-            if(app.config['counter'] == 10):
-                output += "BitCoin Average price for the last 10 minutes: ",(sum(mesurmentsQueue)/len(mesurmentsQueue))
-                counter=0
-        time.sleep(1)
-        return output
 
 # get the latest bitcoin price from URL 
 def get_latest_bitcoin_price():
@@ -45,6 +21,33 @@ def get_latest_bitcoin_price():
     response_json = response.json() 
     # return the current price in USD
     return response_json["bpi"]["USD"]["rate"]
+
+def get_price_avg():
+    global total, counter
+    while time.time() < start_time + 600:
+        counter += 1
+        total += float(latest_price.replace(",", ""))
+        avg = total / counter
+        return avg
+
+def my_value(number):
+    return ("{:,}".format(number))
+
+latest_price = get_latest_bitcoin_price()
+#mesurmentsQueue = deque([]) # que to save last 10 bitcoin price value
+
+app = Flask(__name__)
+
+@app.route("/") # att root display 
+def main():
+    global total, counter, latest_price, mesurmentsQueue
+    #mesurmentsQueue = deque([]) # que to save last 10 bitcoin price value
+    # keep cheking if bitcoin price changed
+    if latest_price != get_latest_bitcoin_price(): # update argumanet if changed
+        latest_price = get_latest_bitcoin_price()
+    output = "The current Price of BitCoin is: " + latest_price + " US $ <br/><br/>"
+    output += "BitCoin Average price for the last 10 minutes: " + my_value(get_price_avg()) + " US $"
+    return output
 
 
 
